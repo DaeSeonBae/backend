6dc1fac4a602c2f139b86e5ef42a1b111e3b4046
Dwisgolmog <rlaehdgus134@naver.com>
2024-03-28 17:22:23 +0900

    Init Spring Boot

---
+++ src/main/java/com/daeseonbae/DSBBackend/DsbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daeseonbae.DSBBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daeseonbae/DSBBackend/DsbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daeseonbae.DSBBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
