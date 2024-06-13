e9eeb7111ccf79f7afedeec7418f75477eb5374a
hhyukk <hhyukk1273@gmail.com>
2024-06-13 19:39:14 +0900

    Add /ai GET

--- src/main/java/com/daeseonbae/DSBBackend/repository/AiRepository.java
+++ src/main/java/com/daeseonbae/DSBBackend/repository/AiRepository.java
@@ -4,6 +4,9 @@ import com.daeseonbae.DSBBackend.entity.AiEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AiRepository extends JpaRepository<AiEntity, Integer> {
+    List<AiEntity> findByUserId(Integer userId);
 }
