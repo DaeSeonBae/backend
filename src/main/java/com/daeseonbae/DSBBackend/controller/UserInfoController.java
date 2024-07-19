7680e88996085ed93551186f0ef9d997b5679914
hhyukk <hhyukk1273@gmail.com>
2024-07-20 01:08:36 +0900

    UserInfoController 삭제 내용 복구

--- src/main/java/com/daeseonbae/DSBBackend/controller/UserInfoController.java
+++ src/main/java/com/daeseonbae/DSBBackend/controller/UserInfoController.java
@@ -5,22 +5,24 @@ import com.daeseonbae.DSBBackend.dto.UserCommentDTO;
 import com.daeseonbae.DSBBackend.entity.UserEntity;
 import com.daeseonbae.DSBBackend.service.UserCommentService;
 import com.daeseonbae.DSBBackend.service.UserInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
 public class UserInfoController {
 
     private final UserInfoService userInfoService;
     private final UserCommentService userCommentService;
 
-    @Autowired
     public UserInfoController(UserInfoService userInfoService, UserCommentService userCommentService) {
         this.userInfoService = userInfoService;
         this.userCommentService = userCommentService;
@@ -31,8 +33,9 @@ public class UserInfoController {
         return userInfoService.getUserInfo(authentication);
     }
 
-    @GetMapping("/comment")
+    @GetMapping("/user/comment")
     public List<UserCommentDTO> getUserComments(Authentication authentication) {
         return userCommentService.getCommentsByUser(authentication);
     }
+
 }
